182119280fff8159771e12d7986034767883552f
a20louba <a20louba@student.his.se>
2022-04-06 14:56:06 +0200

    Initial commit recycleviewdemo myadapter

---
+++ app/src/main/java/com/example/recyclerviewappdemoo/MyViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.recyclerviewappdemoo;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public TextView name;
+
+
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+        name= itemView.findViewById(R.id.name);
+    }
+}
